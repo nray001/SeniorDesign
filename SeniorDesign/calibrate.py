c1a4e2e18673115c340b55fa9055b8c0c8dbe643
nray001 <62325179+nray001@users.noreply.github.com>
2020-03-18 01:53:03 -0700

    Update calibrate.py

--- SeniorDesign/calibrate.py
+++ SeniorDesign/calibrate.py
@@ -2,9 +2,6 @@ import numpy as np
 import cv2
 import glob
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
 # Create object points with varying x and y but keep z = 0
 objp = np.zeros((6*8,3), np.float32)
 objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)*18
